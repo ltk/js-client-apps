396b3baf5810ba56112a059fb7761e990abad82c
Lawson Kurtz <lawson.kurtz@viget.com>
2013-07-02 21:50:43 -0400

    Update article collection comparator for new json timestamps

--- frameworks/backbone/js/app/collections/articles.js
+++ frameworks/backbone/js/app/collections/articles.js
@@ -2,8 +2,16 @@ define(['models/article'], function(Article) {
   return Backbone.Collection.extend({
     url: "/articles",
     model: Article,
-    comparator: function(article) {
-      return -article.get('timestamp');
+    comparator: function(article1, article2) {
+      var article1_timestamp = article1.get('updated_at');
+      var article2_timestamp = article2.get('updated_at');
+
+      if (article1_timestamp > article2_timestamp) {
+        return -1;
+      } else if(article2_timestamp > article1_timestamp) {
+        return 1;
+      }
+      return 0;
     }
   });
 });
